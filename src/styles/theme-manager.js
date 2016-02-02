2681f492c4eedbf1c7e645e48b44eef0df463486
Michael Hahn <mwhahn@gmail.com>
2016-02-01 20:57:47 -0800

    Use react-addons-update $merge instead of lodash.merge
    lodash.merge will mutate whatever object you pass into the function.
    
    If you're overriding styles as specified here:
    http://www.material-ui.com/#/customization/themes you might be passing
    in a reference to `this.context.muiTheme`, which if mutated, will not be
    confined to subcomponents of the current component, but will alter the
    whole tree.

--- src/styles/theme-manager.js
+++ src/styles/theme-manager.js
@@ -1,5 +1,4 @@
 import update from 'react-addons-update';
-import merge from 'lodash.merge';
 import getMuiTheme from './getMuiTheme';
 // import deprecatedExport from '../utils/deprecatedExport';
 
@@ -10,7 +9,7 @@ export default// deprecatedExport(
       return getMuiTheme(update(muiTheme.baseTheme, {spacing: {$set: spacing}}));
     },
     modifyRawThemePalette(muiTheme, palette) {
-      const newPalette = merge(muiTheme.baseTheme.palette, palette);
+      const newPalette = update(muiTheme.baseTheme.palette, {$merge: palette});
       return getMuiTheme(update(muiTheme.baseTheme, {palette: {$set: newPalette}}));
     },
     modifyRawThemeFontFamily(muiTheme, fontFamily) {
